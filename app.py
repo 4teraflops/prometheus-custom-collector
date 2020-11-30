594c78241de7a59a7f5434578b1be904b0855f6d
4teraflops <3lL05WzAr2UA>
2020-11-30 17:45:19 +0500

    + metric

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import time
+import json
+from config import webhook_url, admin_id
 from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
 from prometheus_client import start_http_server
 import requests
 from loguru import logger
+import sqlite3
 
 logger.add(f'log/{__name__}.log', format='{time} {level} {message}', level='DEBUG', rotation='10 MB', compression='zip')
 
@@ -13,10 +16,32 @@ urls = [
 ]
 
 
+def do_alarm(t_alarmtext):
+    headers = {"Content-type": "application/json"}
+    payload = {"text": f"{t_alarmtext}", "chat_id": f"{admin_id}"}
+    requests.post(url=webhook_url, data=json.dumps(payload), headers=headers)
+
+
 def get_data_from_nginx(url):
     s = requests.Session()
     r = s.get(url)
     return r.json()
+
+
+def get_finmonstate():
+    db_path = 'C:/Users/User/PycharmProjects/webhook_server/src/db.sqlite'
+    conn = sqlite3.connect(db_path)  # Инициируем подключение к БД
+    cursor = conn.cursor()
+
+    try:
+        finmonstate = cursor.execute(
+            "SELECT state FROM finmon_states WHERE date_time = (SELECT max(date_time) FROM finmon_states WHERE date_time >= datetime('now', '-2 day'))"
+        ).fetchall()[0][0]
+    except IndexError:
+        finmonstate = 0
+    #logger.info(f'finmonstate: {finmonstate}')
+    conn.commit()
+    return finmonstate
 
 
 @logger.catch()
@@ -25,21 +50,27 @@ class CustomCollector(object):
         pass
 
     def collect(self):
-        g = GaugeMetricFamily("nginx_upstreams", 'Help text', labels=['upstream', 'name'])
+        #  Все доступные аплинки
+        g = GaugeMetricFamily("nginx_upstreams", '1/0 = UP/DOWN', labels=['upstream', 'name'])
         for url in urls:  # Итерация по списку урлов
             request = get_data_from_nginx(url)
-            #logger.info(f'url: {url}\nrequest: {request}')
+            # logger.info(f'url: {url}\nrequest: {request}')
             for server in request["servers"]["server"]:  # Итерация по списку доступных апстримов
-                #logger.info(f'server: {server}')
+                # logger.info(f'server: {server}')
                 upstream = server["upstream"]
                 hostname = server["name"]
                 if server["status"] == 'up':  # заменяем на цифры, чтоб прометей принял
                     state = 1
-                elif server["status"] == 'down':
+                else:
                     state = 0
                 g.add_metric([upstream, hostname], state)
 
-        #g.add_metric(["api.skassa.ru"], 1)
+        #  Состояние загрузки данных по финмон
+        f = GaugeMetricFamily("finmon_status", "1/0 = UP/DOWN")
+        finmon_state = get_finmonstate()
+        f.add_metric([], finmon_state)
+
+        yield f
         yield g
 
 
@@ -51,3 +82,7 @@ if __name__ == '__main__':
             time.sleep(1)
     except KeyboardInterrupt:
         logger.info('Program stopped')
+    except Exception as e:
+        t_alarmtext = f'Crossout_helper (app.py):\n {str(e)}'
+        do_alarm(t_alarmtext)
+        logger.error(f'Other except error Exception', exc_info=True)
